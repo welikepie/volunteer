7a2a6bed15e24d83eeda5bb5661b4170ff430ee2
Charlotte Spencer <charlottelaspencer@gmail.com>
2013-11-07 15:55:02 +0000

    Enabled Description and Contact Details for Events Page.

--- volunteer/public/javascripts/controllers.js
+++ volunteer/public/javascripts/controllers.js
@@ -1,7 +1,21 @@
 function ItemListCtrl ($scope) {
  $scope.london= [
- { "Country": "London", "Event" : "London JS", "URL": "http://www.eventhandler.co.uk"},
- { "Country": "London", "Event" : "GeekyScience", "URL": "http://www.geekyscience.com"},
+ {
+ 	"Country": "London",
+ 	"Event" : "London JS",
+ 	"URL": "http://www.eventhandler.co.uk",
+ 	"Description": "An awesome monthly JavaScript meet-up",
+ 	"Contact" : "contact@eventhandler.co.uk"
+ },
+
+ { 
+ 	"Country": "London",
+ 	"Event" : "GeekyScience",
+ 	"URL": "http://www.geekyscience.com",
+ 	"Description": "Geeky Science, Geeky Scientists! Looking for someone to help out with the checkin for the evening. Will be paid in love, hugs and kisses",
+ 	"Contact" : "charlotte@welikepie.com"
+ },
+
  { "Country": "London", "Event" : "GeekyConf", "URL": "http://www.geekyconf.com"},
  { "Country": "London", "Event" : "London JS Conf", "URL": "http://www.londonjsconf.com"}
  ];
